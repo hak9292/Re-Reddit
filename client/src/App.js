744cbc230e832f525a86fccd250014f07d0c2466
hak9292 <ak112997@gmail.com>
2022-04-05 11:55:48 -0400

    reupdating with my changes except files structure change

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,22 @@ import React from 'react';
 import Header from './components/Header';
 import Subred from './components/Subred';
 import Nav from './components/Nav';
+import Post from './components/Post'
+import axios from 'axios';
+import { useState, useEffect } from "react";
 
 
 export default function App() {
+  useEffect(() => {
+    axios.get('localhost:8080/user', {
+      withCredentials: true
+    })
+      .then(response => SpeechSynthesisUtterance(response.data));
+  }, []);
   return (
     <div>
       <Header />
+      <Post />
       <Subred />
     </div>
   );
