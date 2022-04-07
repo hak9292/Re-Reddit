4407e07c7c00cbdeefa48b15f64d176891d4a500
Sam Rapowitz <samrapow@gmail.com>
2022-04-06 23:02:12 -0400

    created register post request and models folder

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
-import * as mongoose from "mongoose";
+import mongoose from "mongoose";
 import cors from 'cors';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
@@ -12,9 +14,33 @@ app.use(cors({
     credentials: true,
   }));
 
+await mongoose.connect('mongodb://localhost:27017/rereddit', {useNewUrlParser:true,useUnifiedTopology:true,});
+const db = mongoose.connection;
+db.on('error', console.log);
+
 
 app.get('/', (req, res) => {
     res.send('ok');
+  });
+
+  app.post('/register', (req, res) => {
+    const {email,username} = req.body;
+    // encrypt password with bcrypt
+    const password = bcrypt.hashSync(req.body.password, 10);
+    const user = new User({email,username,password});
+    user.save().then(user => {
+      jwt.sign({id:user._id}, secret, (err, token) => {
+        if (err) {
+          console.log(err);
+          res.sendStatus(500);
+        } else {
+          res.status(201).cookie('token', token).send();
+        }
+      });
+    }).catch(e => {
+      console.log(e);
+      res.sendStatus(500);
+    });
   });
 
 app.listen(4000);
